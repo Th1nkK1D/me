44860a24a38214007d4dd7c84b1f959e1cc02df6
Withee Poositasai <witheep@gmail.com>
2017-12-23 14:03:50 +0700

    Remove gh page config

--- config/index.js
+++ config/index.js
@@ -43,13 +43,13 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../me/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../me'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/me/',
-
+    assetsPublicPath: '',
+    
     /**
      * Source Maps
      */
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/me',
+      path: '/',
       name: 'Landing',
       component: Landing
     }
