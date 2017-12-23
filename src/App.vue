2cd53ff9f3ef6f569e7e41c38c81851edafed16e
Withee Poositasai <witheep@gmail.com>
2017-12-23 16:17:43 +0700

    Add skeleton css, simple line icon

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
 </script>
 
 <style lang="scss">
+
 body {
   margin: 0;
   font-family: 'Lato', sans-serif;
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Profile</div>
+  <div class="container">
+    <div class="row">
+      <div class="two columns"><span class="icon-direction"></span></div>
+      <div class="ten columns">View here</div>
+    </div>
+  </div>
 </template>
 
 <script>
